d58995e240a5b8bcfee5628569f121fd178932ac
kumarpritam1468 <manoharipritam@gmail.com>
2024-02-25 21:01:25 +0530

    checkout page half done

---
+++ app.js
@@ -0,0 +1,15 @@
+function oShop(){
+    location.replace('/components/shop.html');
+}
+
+function oBlog(){
+    location.replace('/components/blog.html');
+}
+
+function oHome(){
+    location.replace('../index.html');
+}
+
+function oCheckOut(){
+    location.replace('/components/checkOut.html');
+}
---
+++ myShop/app.js
@@ -0,0 +1,11 @@
+function oShop(){
+    location.replace('/components/shop.html');
+}
+
+function oBlog(){
+    location.replace('/components/blog.html');
+}
+
+function oHome(){
+    location.replace('../index.html');
+}
